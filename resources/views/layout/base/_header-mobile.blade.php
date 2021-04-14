d5d1d456c64be0bb1f6e6672dae853d208d74189
buruhsd <buruhsd@outlook.co.id>
2021-04-14 15:24:13 +0700

    logo

--- resources/views/layout/base/_header-mobile.blade.php
+++ resources/views/layout/base/_header-mobile.blade.php
@@ -25,7 +25,7 @@
 
             @endif
 
-            <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/>
+            <!-- <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/> -->
         </a>
     </div>
     <div class="d-flex align-items-center">
