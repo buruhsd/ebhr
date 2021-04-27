9607a6c21be5df55e34cc308b1776f6d48c4febf
buruhsd <buruhsd@outlook.co.id>
2021-04-27 12:49:28 +0700

    get

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('employee', 'HrController@getHRdata');
-Route::post('identity/list/json', [EmployeeController::class, 'getIdentityCard'])->name('api.employee.getIdentityCard');
+Route::get('identity/list/json', [EmployeeController::class, 'getIdentityCard'])->name('api.employee.getIdentityCard');
