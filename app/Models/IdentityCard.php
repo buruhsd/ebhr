154e337ea9216011a2837533211de354256fe3de
buruhsd <buruhsd@outlook.co.id>
2021-06-09 13:42:59 +0700

    mass asignment

--- app/Models/IdentityCard.php
+++ app/Models/IdentityCard.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class IdentityCard extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nik',
+        'name',
+        'gender',
+        'date_of_birth',
+        'blood_type',
+        'religion_id',
+        'work_type_id',
+        'nationality',
+        'marital_status_id',
+        'address',
+        'rt',
+        'rw',
+        'postal_code_id',
+        'village_id',
+        'district_id',
+        'regency_id',
+        'province_id',
+        'published_date_ktp',
+        'description',
+        'insertedBy',
+        'updatedBy',
+    ];
 }
