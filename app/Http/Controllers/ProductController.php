c0ab082403963511621ab928c837dcde67bdd0ef
buruhsd <buruhsd@outlook.co.id>
2021-07-04 15:04:46 +0700

    up

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,16 +9,9 @@ class ProductController extends Controller
 {
     public function index(Request $request){
         $search = $request->search;
-        $sortBy = $request->input('sortby');
-        $orderBy = $request->input('orderby');
-        if(is_null($orderBy)){
-            $orderBy = 'name';
-        }
-        if(is_null($sortBy)){
-            $sortBy = 'asc';
-        }
+
         return Product::Where('name', 'LIKE',"{$search}%")
-            ->orderBy($orderBy, $sortBy)->paginate(20);
+                        ->orWhere('product_code', 'LIKE',"{$search}%")->limit(10)->get();
     }
 
     public function store(){
