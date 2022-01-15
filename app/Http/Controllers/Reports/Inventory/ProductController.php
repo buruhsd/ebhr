4ab7460eb9deff7d9d8f1a523f31882b73bf185e
ilhamtuut <ilham.ahmad14@gmail.com>
2022-01-15 11:43:56 +0700

    auth api

--- app/Http/Controllers/Reports/Inventory/ProductController.php
+++ app/Http/Controllers/Reports/Inventory/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     public function index(Request $request)
