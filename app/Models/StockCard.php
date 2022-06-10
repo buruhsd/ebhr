a862f800ed0ff3d756d8bc968cfe4d4a8b3808d6
buruhsd <buruhsd@outlook.co.id>
2022-06-10 10:01:27 +0700

    update stock card

--- app/Models/StockCard.php
+++ app/Models/StockCard.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class StockCard extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'trx_code',
+        'trx_urut',
+        'trx_date',
+        'trx_jenis',
+        'trx_dbcr',
+        'scu_code',
+        'inv_code',
+        'loc_code',
+        'statusProduct',
+        'trx_kuan',
+        'hargaSatuan',
+        'trx_amnt',
+        'trx_totl',
+        'trx_hpok',
+        'trx_havg',
+        'pos_date',
+        'sal_code'
+    ];
 }
