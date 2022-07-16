2330d72781678b56228478399fc70af9062ff344
buruhsd <buruhsd@outlook.co.id>
2022-07-16 11:02:04 +0700

    update stock card

--- app/Http/Controllers/Inventory/ReturnItemController.php
+++ app/Http/Controllers/Inventory/ReturnItemController.php
@@ -82,6 +82,31 @@ class ReturnItemController extends Controller
             if($value['qty'] > $check_data->qty){
                 return response()->json(['success' => false, 'message' => 'Jumlah KG tidak boleh melebihi dari jumlah BPB']);
             }
+
+            try {
+                $stock = StockCard::create([
+                    'trx_code' => '',
+                    'trx_urut' => $itemvalue->id,
+                    'trx_date' => $request->date,
+                    'trx_jenis' => 'BK',
+                    'trx_dbcr' => 'D',
+                    'scu_code' => NULL,
+                    'scu_code_tipe' => NULL,
+                    'inv_code' => $value['no_register'],
+                    'loc_code' => $request->warehouse_id,
+                    'statusProduct' => $item['product_status_id'],
+                    'trx_kuan' => $value['qty'],
+                    'hargaSatuan' => 0,
+                    'trx_amnt' => 0,
+                    'trx_totl' => 0,
+                    'trx_hpok' => 0,
+                    'trx_havg' => 0,
+                    'pos_date' => '-',
+                    'sal_code' => '-'
+                ]);
+            } catch (\Throwable $th) {
+                return;
+            }
         }
 
         $request->merge([
